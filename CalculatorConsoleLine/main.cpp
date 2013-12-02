30060bb4f3368d18bbb704387e4b2c3d1684392d
muhdmirzamz <muhdmirzamz@gmail.com>
2013-12-02 13:42:45 +0800

    First commit

---
+++ CalculatorConsoleLine/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  CalculatorConsoleLine
+//
+//  Created by Muhd Mirza on 2/12/13.
+//  Copyright (c) 2013 Muhd Mirza. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
