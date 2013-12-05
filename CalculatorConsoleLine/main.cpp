7f7642057682fcca8c7d89fcc1db753d6de0c370
muhdmirzamz <muhdmirzamz@gmail.com>
2013-12-05 23:34:40 +0800

    Added loop support for main menu

--- CalculatorConsoleLine/main.cpp
+++ CalculatorConsoleLine/main.cpp
@@ -12,35 +12,76 @@
 
 using namespace std;
 
+void loopAgain() {
+    cout << "Enter your choice (Type word to the left): ";
+    string choiceTwo;
+    cin >> choiceTwo;
+    
+	Loop loop;
+    if (choiceTwo == "Add" || choiceTwo == "add") {
+        cout << loop.add();
+    }
+	
+	if (choiceTwo == "Sub" || choiceTwo == "sub") {
+		cout << loop.subtract();
+	}
+	
+	if (choiceTwo == "Mul" || choiceTwo == "mul") {
+		cout << loop.multiply();
+	}
+	
+	if (choiceTwo == "Div" || choiceTwo == "div") {
+		cout << loop.divide();
+	}
+	
+	if (
+		choiceTwo != "Add" && choiceTwo != "add" &&
+		choiceTwo != "Sub" && choiceTwo != "sub" &&
+		choiceTwo != "Mul" && choiceTwo != "mul" &&
+		choiceTwo != "Div" && choiceTwo != "div"
+		) {
+		loopAgain();
+	}
+}
+
 int main(int argc, const char * argv[])
 {
     cout << "Calculator" << endl;
     
-    string menuArray[] = {"A - Add", "S - Subtract", "M - Multiply", "D - Divide"};
+    string menuArray[] = {"Add - Add", "Sub - Subtract", "Mul - Multiply", "Div - Divide"};
     cout << endl;
     for (int i = 0; i < 4; i++) {
         cout << menuArray[i] << endl;
     }
     
-    cout << "Enter your choice (Type letter): ";
-    char choice;
+    cout << "Enter your choice (Type word to the left): ";
+    string choice;
     cin >> choice;
     
 	Loop loop;
-    if (choice == 'A' || choice == 'a') {
+    if (choice == "Add" || choice == "add") {
         cout << loop.add();
     }
 	
-	if (choice == 'S' || choice == 's') {
+	if (choice == "Sub" || choice == "sub") {
 		cout << loop.subtract();
 	}
 	
-	if (choice == 'M' || choice == 'm') {
+	if (choice == "Mul" || choice == "mul") {
 		cout << loop.multiply();
 	}
 	
-	if (choice == 'D' || choice == 'd') {
+	if (choice == "Div" || choice == "div") {
 		cout << loop.divide();
+	}
+	
+	if (
+		choice != "Add" && choice != "add" &&
+		choice != "Sub" && choice != "sub" &&
+		choice != "Mul" && choice != "mul" &&
+		choice != "Div" && choice != "div"
+	) {
+		loopAgain();
 	}
     return 0;
 }
