537cf0f1f5dcf1fc395420665ff3a042ba6fc4c3
muhdmirzamz <muhdmirzamz@gmail.com>
2013-12-09 19:19:02 +0800

    Added loop support in separate components

---
+++ CalculatorConsoleLine Tests/CalculatorConsoleLine_Tests.m
@@ -0,0 +1,34 @@
+//
+//  CalculatorConsoleLine_Tests.m
+//  CalculatorConsoleLine Tests
+//
+//  Created by Muhd Mirza on 9/12/13.
+//  Copyright (c) 2013 Muhd Mirza. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CalculatorConsoleLine_Tests : XCTestCase
+
+@end
+
+@implementation CalculatorConsoleLine_Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
